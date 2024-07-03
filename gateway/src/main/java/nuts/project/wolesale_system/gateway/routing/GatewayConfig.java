163eba14409e60ec5901e64863492ff4f880fd8a
MIMminE <weluv.ww@gmail.com>
2024-07-03 16:47:34 +0900

    [ gateway ] 게이트웨이 서비스 추가

--- gateway/src/main/java/nuts/project/wolesale_system/gateway/routing/GatewayConfig.java
+++ gateway/src/main/java/nuts/project/wolesale_system/gateway/routing/GatewayConfig.java
@@ -1,5 +1,9 @@
 package nuts.project.wolesale_system.gateway.routing;
 
+import com.nimbusds.jose.JOSEException;
+import nuts.lib.manager.security_manager.authentication.token.jwt.JwtTokenService;
+import nuts.lib.manager.security_manager.authentication.token.jwt.JwtTokenServiceBuilder;
+import nuts.lib.manager.security_manager.authentication.token.jwt.algoritim.HMACSupport;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,15 +13,26 @@ import java.net.URI;
 
 @Configuration
 public class GatewayConfig {
-//
-//    @Bean
-//    RouteLocator routeLocator(RouteLocatorBuilder builder){
-//        return builder.routes()
-//                .route("/test", r-> r.path("/test")
-//                        .filters(f->f.filter((exchange, chain) -> {
-//                            System.out.println(exchange.getRequest().getHeaders());
-//                            return chain.filter(exchange);
-//                        })).uri(URI.create("http://localhost:9004/orders/harunuts"))).build();
-//    }
+
+    @Bean
+    RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("/test", r -> r.path("/orders/harunuts")
+                        .filters(f -> f.filter((exchange, chain) -> {
+                            System.out.println(exchange.getRequest().getHeaders());
+                            return chain.filter(exchange);
+                        })).uri(URI.create("http://localhost:9004/")))
+
+                .route("token", r -> r.path("/token").uri("http://localhost:9001/"))
+                .build();
+    }
+
+    @Bean
+    JwtTokenService jwtTokenService() throws JOSEException {
+        return new JwtTokenServiceBuilder()
+                .HMACSigner(HMACSupport.HS256, "hellohellohellohellohellohellohellohellohellohellohello").build();
+    }
+
+
 }
 
