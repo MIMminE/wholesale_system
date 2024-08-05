634881ce703152d6e27dce7cd890c1a4e3c00f65
MIMminE <weluv.ww@gmail.com>
2024-08-05 13:56:16 +0900

    [ authentication ] 임시 인증 서비스 구현( 실제 인증 제공 X )

---
+++ authentication/src/main/java/nuts/project/wholesale_system/authentication/config/Config.java
@@ -0,0 +1,14 @@
+package nuts.project.wholesale_system.authentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ authentication/src/main/java/nuts/project/wholesale_system/authentication/service/AuthenticationService.java
@@ -0,0 +1,42 @@
+package nuts.project.wholesale_system.authentication.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final RestTemplate restTemplate;
+
+    private String authServerUrl = "http://localhost:8080/test";
+
+    /**
+     * It communicates with the authentication server and issues a token
+     *
+     * @param username The username that you send to the authentication server
+     * @param password Passwords sent to the authentication server
+     *                                 TODO
+     * @return Token Info Map
+     */
+    public Map<String, Object> requestToken(String username, String password) {
+
+
+        return restTemplate.postForObject(authServerUrl + "/token", null, Map.class);
+    }
+
+
+    /**
+     * Retrieve the user information registered on the authentication server
+     * TODO
+     *
+     * @return userInfo
+     */
+    public Map<String, Object> getUserTable() {
+        return restTemplate.getForObject(authServerUrl + "/users", Map.class);
+    }
+}
